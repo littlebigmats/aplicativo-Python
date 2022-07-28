3a0fea581ef72b0c02825fcc5dcc4fa6add8f130
littlebigmats <matias.sugranes@gmail.com>
2022-07-28 00:33:24 +0000

    SQL Alchemy setup

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
-from flask import Flask, render_template
-# importando de flask (biblioteca) la clas Flas (clases son con mayúscula)
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
-# importando datetime para dados do tipo tempo
-app = Flask ('hello')
-# hello es el nombre de la app
-posts=[
-    {
-        'title': 'O meu primeiro post',
-        'body' : 'Aqui é o texto do post',
-        'author': 'Feulo',
-        'created' : datetime(2022,7,25)
-    },
-    {
-        'title': 'O meu segundo post',
-        'body' : 'Aqui é o texto do post',
-        'author': 'Danilo',
-        'created' : datetime(2022,7,26)
-    }
-]
-#post é uma lista [] com diccionario {}
-@app.route('/')
-# método ou funçao para criar rota para o endpoint, precisa de @
-def index(): 
-    return render_template('index.html', posts = posts)
-    # def hello é um recurso criado dentro da rota    
-@app.route('/login')
-def login():
-    return render_template("login.html")
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
+
+@app.route("/")
+def index():
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
+
+@app.route("/populate")
+def populate():
+    user = User(username='feulo', email="g@g.com", password_hash='a')
+    post1 = Post(title="Post 1", body="Texto do Post", author=user)
+    post2 = Post(title="Post 2", body="Texto do Post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
+
